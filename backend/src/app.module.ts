efe8f0dcd9910cccf1e44bd3f61d79e64ebb5c2f
Eduardo <edu4rdobarb0za@gmail.com>
2026-04-22 10:28:46 -0300

    fix(health): revert middleware exclude path to health

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -32,7 +32,7 @@ export class AppModule implements NestModule {
       .apply(ApiKeyMiddleware)
       .exclude(
         { path: 'auth/(.*)', method: RequestMethod.ALL },
-        { path: 'api/health', method: RequestMethod.GET },
+        { path: 'health', method: RequestMethod.GET },
       )
       .forRoutes('*');
   }
